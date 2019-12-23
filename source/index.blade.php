1b4a8db31e1a4ec204a6a20509a07922de7d4d4f
Daiane Alves <daiane@php.rio>
2019-12-23 12:03:34 -0300

    Instalação do jigsaw

--- source/index.blade.php
+++ source/index.blade.php
@@ -1,7 +1,6 @@
----
-layout: default
----
-{% include banner.html %}
-{% include mission.html %}
-{% include event_list.html %}
-{% include highlights.html %}
+@extends('_layouts.master')
+
+@include('_layouts.master.banner')
+@include('_layouts.master.mission')
+@include('_layouts.master.event_list')
+@include('_layouts.master.highlights')
