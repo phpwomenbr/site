09a3092bcef3f0f102f324cb70b62aa54629d636
Daiane Alves <daiane@phprio.org>
2022-05-11 18:29:00 -0300

    Atualização de pacotes

--- webpack.mix.js
+++ webpack.mix.js
@@ -5,6 +5,13 @@ mix.disableSuccessNotifications();
 mix.setPublicPath('source/assets/build');
 
 mix.jigsaw()
+    .browserSync({
+        server: 'build_local',
+        files: ['build_*/**'],
+        open: false,
+    });
+
+mix.jigsaw()
     .js('source/_assets/js/main.js', 'js')
     .sass('source/_assets/sass/main.scss', 'css')
     .options({
